6dacc88543918a87bbe2ec16e04813379ed2a84e
Sshrutisri123 <168011388+Sshrutisri123@users.noreply.github.com>
2025-02-20 12:50:00 +0530

    changed ui of signup page

--- frontend/src/pages/Home/Signup/Signup.jsx
+++ frontend/src/pages/Home/Signup/Signup.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import Practice from '../../../assets/logo/Practice.jpg'; // Logo image
-import logoImage from '../../../assets/logo/logo.png'; // Logo image
+import Logo1 from '../../../assets/logo/logo1.jpg';
 import { FcGoogle } from "react-icons/fc";
 import { GrApple } from "react-icons/gr";
 import { useNavigate } from 'react-router-dom';
@@ -54,97 +53,93 @@ const Signup = () => {
 
   return (
 
-    <div className='flex bg-gradient-to-r from-[#F8A128] to-[#F5F5F5] h-screen py-14 px-28'>
+    <div className='flex justify-between bg-gray-100 h-screen py-14 px-28'>
+      <div className='flex flex-col justify-center items-center h-full w-full '>
 
-      <div className='flex h-full w-full bg-white rounded-[35px] drop-shadow-[5px_5px_6px_rgba(0,0,0,0.2)]'>
-        {/*LEFT SIDE*/}
-        <div className='flex justify-center items-center w-[40%] h-full p-5'>
-          <img className='h-full w-full rounded-2xl' src={Practice} alt="logo" />
+        <div className='flex justify-center items-center gap-2 mb-6'>
+          <img className='size-8 rounded-lg' src={Logo1} alt="Logo image" />
+          <span className="text-2xl font-Logo font-semibold">
+            Notify
+          </span>
         </div>
-        {/*RIGHT SIDE*/}
-        {/*  LOGO  */}
+        {/* Right Side */}
+        <div className='flex border flex-col rounded-xl shadow-md bg-white justify-center items-center  py-5 px-10'>
 
-        <div className='flex flex-col justify-center items-center w-3/5 h-full p-5'>
-          <div className=' flex justify-center items-center'>
-            <img className='size-12' src={logoImage} alt="logo" />
+          {/* header div */}
+          <div className='flex flex-col justify-between items-center'>
 
-
-            <span className=" text-2xl font-Logo ml-[-7.4%] ">
-              otetify
-            </span>
-          </div>
-
-          {/*FILL*/}
-          <div className='flex flex-col items-center gap-6'>
-            <h1 className='font-semibold text-4xl font-instumrntalSans'>
-              Keep Your Notes Organized
-            </h1>
-
-            <div className=' flex flex-col items-center' >
-              <h1 className='font-medium tracking-tight text-4xl font-instumrntalSans'>
-                Welcome Back
+            {/* Welcome Back */}
+            <div className='flex flex-col items-center'>
+              <h1 className="font-medium text-xl tracking-normal">
+                Welcome
               </h1>
-              <p className="text-[#7C7B7B] text-center mb-4 text-sm">Enter your Email and password to access your notes account</p>
-
+              <p className="text-center text-[#7C7B7B] my-2 text-sm">Enter your Email and password to access Notify</p>
             </div>
+
           </div>
 
+          {/* Form div */}
+          <div className='flex flex-col mt-5 gap-3'>
+          <div >
+              <h3 className='text-sm'>Name</h3>
+              <input className='border w-80 rounded-lg h-7 p-4 mt-2 text-sm ' type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)} placeholder='enter your name'/>
+            </div>
+            <div >
+              <h3 className='text-sm'>Email</h3>
+              <input className='border w-80 rounded-lg h-7 p-4 mt-2 text-sm ' type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)} placeholder='m@example.com' />
+            </div>
 
-          {/*FORM*/}
-          <div className='flex flex-col gap-4'>
-            <input className='border rounded-3xl border-[#A09F9F] w-96 h-11 p-4' type="text" value={name}
-              onChange={(e) => setName(e.target.value)} placeholder='Name' />
-            <input className='border rounded-3xl border-[#A09F9F] w-96 h-11 p-4' type="text" value={email}
-              onChange={(e) => setEmail(e.target.value)} placeholder='Email' />
-
-
-            <input className='border rounded-3xl border-[#A09F9F] w-96 h-11 p-4' type="password" value={password}
-              onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
+            <div>
+              <div className='flex justify-between'>
+                <h3 className='text-sm'>Password</h3>
+                
+              </div>
+              <input className='border w-80 rounded-lg h-7 p-4 mt-2 text-sm' type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
+            </div>
 
 
             {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
 
 
+            <button onClick={handleSubmit} className=' flex justify-center items-center w-80 rounded-lg h-7 p-4 bg-black text-white font-normal mt-2 '>Sign Up</button>
 
-
-
-            <button onClick={handleSubmit} className='w-96 rounded-3xl h-11 p-4 bg-my-yellow text-white font-semibold flex items-center justify-center'>Sign In</button>
             <div className='flex justify-center items-center gap-3'>
-              <hr className='w-32  border-[#A09F9F] border' />
-              <span className='text-[#827E7E] tracking-tight'>Or Login With</span>
-              <hr className='w-32  border-[#A09F9F] border' />
+              <hr className='w-24 border-[#A09F9F] border-1' />
+              <span className='text-gray-500 text-sm font-light tracking-tight'>Or login with</span>
+              <hr className='w-24 border-[#A09F9F] border-1' />
             </div>
-            {/*GOOGLE APPLE*/}
-            <div className='flex justify-center items-center gap-4'>
-              <button>
-                <FcGoogle className='size-9' />
+
+            <div className='flex flex-col justify-center items-center gap-3'>
+              <button className='flex gap-2 justify-center items-center border border-gray-300 w-80 rounded-lg h-7 p-4 text-sm hover:bg-gray-100'>
+                <FcGoogle className='size-5' />Login with Google
               </button>
 
-              <button>
-                <GrApple className='size-9' />
+              <button className='flex gap-2 justify-center items-center border border-gray-300 w-80 rounded-lg h-7 p-4 text-sm hover:bg-gray-100'>
+                <GrApple className='size-5' />Login with apple
               </button>
             </div>
+
             <div className="flex items-center justify-center mt-4">
               <p className="text-sm font-normal text-[#827E7E]">
-                Don't have an account?{" "}
+                Already have an account?{" "}
                 <span onClick={handleLoginRedirect}
-                  className="font-semibold text-black cursor-pointer"
+                  className="font-semibold text-black cursor-pointer underline "
                 >
-                  Sign Up
+                  Sign In
                 </span>
               </p>
             </div>
 
-
-
           </div>
-
 
         </div>
       </div>
-
-
-
     </div>
   );
 };
